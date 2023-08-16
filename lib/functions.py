ad6431f5c58935c7aee6eecd62bc839fc6b72010
Bakhita87 <bakhitaotieno@gmail.com>
2023-08-16 19:32:30 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello Programmer")
 
 def greet(name):
-    pass
+    print("Hello", {name})
+    greet("Guido")
 
 def greet_with_default(name="programmer"):
     pass
 
 def add(num1, num2):
-    pass
+    
 
 def halve(number):
     pass
+
